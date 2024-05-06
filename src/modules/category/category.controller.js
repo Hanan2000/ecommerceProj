886bfd9480d792132e28740a55270ec478fd5e91
Hanan2000 <hanan2000khalid@gmail.com>
2024-05-07 00:12:26 +0300

    CRUD Category

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -13,3 +13,52 @@ export const create =async(req,res)=>{
     const category = await categoryModel.create({name,slug:slugify(name),image:{secure_url,public_id}});
     return res.json({message:category});
 };
+
+export const getAll = async(req,res)=>{
+ const categories = await categoryModel.find({});
+ return res.status(200).json({message:categories});
+};
+export const getActive = async(req,res)=>{
+    const categories = await categoryModel.find({status:'Active'}).select("name");
+    return res.status(200).json({message:categories});
+   };
+   export const getDetails = async(req,res)=>{
+    const category = await categoryModel.findById(req.params.id);
+    return res.status(200).json({message:category});
+   };
+
+   export const update = async(req,res)=>{
+    const category = await categoryModel.findById(req.params.id);
+    if(!category){
+        return res.status(404).json({message:"category not found"});
+    }
+    category.name=req.body.name.toLowerCase();
+    if(await categoryModel.findOne({name:req.body.name,_id:{$ne:req.params.id}})){
+        return res.status(409).json({message:"name already exists"});
+    }
+    category.slug=slugify(req.body.name);
+    if(req.file){
+        const {secure_url,public_id} = await cloudinary.uploader.upload(req.file.path,{
+            folder:'tshop5/categories'
+        });
+        cloudinary.uploader.destroy(category.image.public_id);
+        category.image={secure_url,public_id};
+    }
+     category.status=req.body.status;
+     await category.save();
+    return res.json({message:"success",category});
+   };
+
+   export const destroy = async(req,res)=>{
+    const category = await categoryModel.findByIdAndDelete(req.params.id);
+    if(!category){
+        return res.status(404).json({message:"catagory not found "});
+    }
+    await  cloudinary.uploader.destroy(category.image.public_id);
+    return res.status(200).json({message:"success",category});
+   };
+
+
+   
+   
+   
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -4,7 +4,11 @@ import * as Controller from './category.controller.js';
 import fileUpload ,{fileType} from './../../utls/multer.js';
 
 router.post('/',fileUpload(fileType.image).single('image'),Controller.create);
-
+router.get('/',Controller.getAll);
+router.get('/active',Controller.getActive);
+router.get('/:id',Controller.getDetails);
+router.patch('/:id',fileUpload(fileType.image).single('image'),Controller.update);
+router.delete('/:id',Controller.destroy);
 
 
 export default router;
